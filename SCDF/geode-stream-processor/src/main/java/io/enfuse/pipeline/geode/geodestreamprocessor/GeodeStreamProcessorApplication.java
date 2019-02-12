236dc2df4e1480267f8cd88f47a9b385471c42f1
Cahlen Humphreys <cahlen@gmail.com>
2019-02-11 23:18:08 -0700

    Add initial gradle SCS build for processor
    Adding the initial vanilla build with spring cloud stream and rabbitmq
    integration for the stream processor application.

---
+++ SCDF/geode-stream-processor/src/main/java/io/enfuse/pipeline/geode/geodestreamprocessor/GeodeStreamProcessorApplication.java
@@ -0,0 +1,14 @@
+package io.enfuse.pipeline.geode.geodestreamprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeodeStreamProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeodeStreamProcessorApplication.class, args);
+    }
+
+}
+
---
+++ SCDF/geode-stream-processor/src/test/java/io/enfuse/pipeline/geode/geodestreamprocessor/GeodeStreamProcessorApplicationTests.java
@@ -0,0 +1,17 @@
+package io.enfuse.pipeline.geode.geodestreamprocessor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeodeStreamProcessorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
